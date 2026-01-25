b936cfde4eb2095108569d82ba94c17e0130ee93
Galand Noah <galandnoah@gmail.com>
2026-01-25 20:44:17 +0100

    Initialization

---
+++ src/main/java/galand/projects/produits_jardinage/ProduitsJardinageApplication.java
@@ -0,0 +1,13 @@
+package galand.projects.produits_jardinage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitsJardinageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitsJardinageApplication.class, args);
+	}
+
+}
