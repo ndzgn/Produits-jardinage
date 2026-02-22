68195398626621a0826c30238e65dc423d959d86
Galand Noah <galandnoah@gmail.com>
2026-02-22 12:22:52 +0100

    initialize the unit test with mockito

---
+++ src/test/java/galand/projects/produits_jardinage/service/ProductServiceImplTest.java
@@ -0,0 +1,59 @@
+package galand.projects.produits_jardinage.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Test unitaire avec Mockito
+ * */
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void getProductByID() {
+    }
+
+    @Test
+    void getAllActiveProduct() {
+    }
+
+    @Test
+    void getProducstByCategory() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void adjustStock() {
+    }
+
+    @Test
+    void applyDiscount() {
+    }
+
+    @Test
+    void getOutOfStockProducts() {
+    }
+
+    @Test
+    void getLowStockProducts() {
+    }
+
+    @Test
+    void validatePrice() {
+    }
+}
