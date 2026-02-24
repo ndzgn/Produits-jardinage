67e11c0c60922515b17432c25880889a31613d76
Galand Noah <galandnoah@gmail.com>
2026-02-24 12:31:59 +0100

    Add pagination and filter methods

--- src/main/java/galand/projects/produits_jardinage/service/ProductServiceImpl.java
+++ src/main/java/galand/projects/produits_jardinage/service/ProductServiceImpl.java
@@ -13,6 +13,10 @@ import galand.projects.produits_jardinage.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
