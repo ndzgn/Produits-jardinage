9a4005fd2150eb4c94e02f2413cca95e2b5dcc43
Galand Noah <galandnoah@gmail.com>
2026-02-24 12:24:56 +0100

    INSERT VALUE into the product table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO PRODUCT (name, category, price, stock, description,active)
+VALUES
+('Rosier', 'PLANTE', 4000, 50, 'Plante de rosier', true),
+('Potassium', 'ENGRAIS', 6000, 60, 'Engrais util pour les plantes', true),
+('Compost naturel', 'ENGRAIS', 7000, 20, 'Melange de matieres organiques decomposees', true),
+('Engrais NPK 8-8-8 organique', 'ENGRAIS', 10000, 100, 'Engrais equilibre contenat de l''azote du phosphore et du potassium', true),
+('Engrais granule au fumier de poule', 'ENGRAIS', 8000, 30, 'Engrais organique a liberation lente', true),
+('Menthe', 'PLANTE', 6000, 56, 'Plantes aromatiques', true),
+('Jasmin', 'PLANTE', 4000, 50, 'Plantes grimpantes', true),
+('Basilic', 'PLANTE', 5000, 37, 'Plantes aromatiques', true),
+('Secateur', 'OUTIL', 10000, 70, 'Outil coupant pour tailler branches et tiges', true),
+('Truelle de jardin', 'OUTIL', 7000, 60, 'Petite pelle a main sert a treuser des trous , transplanter ou enlever les mauvaises herbes facilement', true),
+('Arrosoir', 'ACCESSOIRE', 2000, 50, 'Recipient muni d''un bec pour arroser les plantes', true);
