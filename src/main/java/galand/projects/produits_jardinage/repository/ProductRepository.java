bdc32571c5d7b560b301e2906f5cc1bbb6bdc699
Galand Noah <galandnoah@gmail.com>
2026-02-24 12:29:31 +0100

    add personnalized jpql methods for pagination and sorting

--- src/main/java/galand/projects/produits_jardinage/repository/ProductRepository.java
+++ src/main/java/galand/projects/produits_jardinage/repository/ProductRepository.java
@@ -2,9 +2,13 @@ package galand.projects.produits_jardinage.repository;
 
 import galand.projects.produits_jardinage.entity.Product;
 import galand.projects.produits_jardinage.entity.ProductCategory;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -36,4 +40,11 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     //Requete Native SQL
     @Query(value = "SELECT * FROM product WHERE price > :minPrice AND stock > 0", nativeQuery = true)
     List<Product> findAvailableProductsAbovePrice(@Param("minPrice") BigDecimal minPrice);
+
+
+    //Filtrer les produits qui sont dans un intervalle de prix et qui appartiennent a une categorie bien defien
+    @Query(value = "SELECT * FROM product WHERE (price BETWEEN :min AND :max) AND category = :category", nativeQuery = true)
+    Page<Product> filterByCategoryAndPrice(@Param("min") BigDecimal min, @Param("max") BigDecimal max, @Param("category") String category, Pageable pageable);
+
+    Page<Product> findByCategory(String category, Pageable pageable); //Pageable: l'ordre dans lequel on va veut trier et le nombre d'elements a afficher sur la page
 }
